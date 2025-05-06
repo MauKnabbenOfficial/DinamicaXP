e097c496647820ef66dde73199da8ccdcf44b8c1
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2025-05-06 18:49:46 -0300

    #5 - sprint 3: Adicionado banco de dados

---
+++ Migrations/20250506210700_AddModelCreating.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DinamicaXP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModelCreating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_contas_clientes_cliente_id",
+                table: "contas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_pagamentos_clientes_cliente_id",
+                table: "pagamentos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cliente_id",
+                table: "pagamentos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cliente_id",
+                table: "contas",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "clientes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "conta_id",
+                table: "clientes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_contas_clientes_cliente_id",
+                table: "contas",
+                column: "cliente_id",
+                principalTable: "clientes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pagamentos_clientes_cliente_id",
+                table: "pagamentos",
+                column: "cliente_id",
+                principalTable: "clientes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_contas_clientes_cliente_id",
+                table: "contas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_pagamentos_clientes_cliente_id",
+                table: "pagamentos");
+
+            migrationBuilder.DropColumn(
+                name: "conta_id",
+                table: "clientes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cliente_id",
+                table: "pagamentos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cliente_id",
+                table: "contas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "clientes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_contas_clientes_cliente_id",
+                table: "contas",
+                column: "cliente_id",
+                principalTable: "clientes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pagamentos_clientes_cliente_id",
+                table: "pagamentos",
+                column: "cliente_id",
+                principalTable: "clientes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -12,6 +12,7 @@ namespace DinamicaXP.Models
     public class Cliente
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Column("id")]
         public int Id { get; set; }
 
@@ -19,8 +20,12 @@ namespace DinamicaXP.Models
         [Column("name")]
         public string Name { get; set; }
 
-        public ICollection<Pagamento> Pagamentos { get; set; }
-        public Conta Conta { get; set; }
+        [ForeignKey("Conta")]
+        [Column("conta_id")]
+        public int? ContaId { get; set; }
+
+        public virtual Conta Conta { get; set; }
+        public virtual ICollection<Pagamento> Pagamentos { get; set; }
     }
 }
 
